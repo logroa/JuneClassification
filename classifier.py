e3ce5fe17f949e82f4a2f7f4115adf2c49469a6a
Logan <logroa@umich.edu>
2021-07-01 09:45:07 -0500

    Improvements on Gradient Boosting

--- classifier.py
+++ classifier.py
@@ -86,19 +86,16 @@ plt.title('Distribution of yi in train data')
 plt.grid()
 plt.show()
 
-
-# feature importance
-
 from xgboost import XGBClassifier
 from xgboost import plot_importance
 from matplotlib import pyplot
 
-#pyplot.figure(figsize=(50,30))
-#model = XGBClassifier()
-#model.fit(x_train, y_train)
-#plot_importance(model, max_num_features=25)
+pyplot.figure(figsize=(50,30))
+model = XGBClassifier()
+model.fit(x_train, y_train)
+plot_importance(model, max_num_features=25)
 
-#pyplot.show()
+pyplot.show()
 
 a = [10 ** x for x in range(-5,1)]
 
@@ -127,8 +124,6 @@ plt.xlabel("Alpha")
 plt.ylabel("Error")
 plt.show()
 
-
-
 ba = np.argmin(logerrorCV)
 c = SGDClassifier(alpha = a[ba], penalty = 'l2', loss = 'log', random_state=42, class_weight="balanced")
 c.fit(x_train, y_train.values.ravel())
@@ -146,7 +141,6 @@ print('Best alpha = ', a[ba], '; cv log loss: ',
 pred_y = sc.predict_proba(X_test)
 print('Best alpha = ', a[ba], '; test log loss: ',
       log_loss(Y_test, pred_y, labels=c.classes_, eps=1e-15))
-
 
 exam = exam.drop('id', axis=1)
 
@@ -172,50 +166,66 @@ turner = pd.DataFrame(outpA,
                                'Class_4', 'Class_5', 'Class_6', 'Class_7', 
                                'Class_8', 'Class_9'])
 turner.to_csv(path_or_buf='submission.csv', index = False)
-turner
+
+from lightgbm import LGBMClassifier
+
+a = [200]
+md = [2, 3, 5, 10]
+
+logerrorCV = []
+for i in a:
+    for j in md:
+        print("n-estimators: ", i, "max depth: ", j)
+        c = LGBMClassifier(n_estimators=i,
+                           max_depth=j,
+                           random_state=42,
+                           n_jobs=-1)
+        c.fit(x_train, y_train.values.ravel())
+        sc = CalibratedClassifierCV(c, method="sigmoid")
+        sc.fit(x_train, y_train.values.ravel())
+        scProb = sc.predict_proba(x_cv)
+        logerrorCV.append(log_loss(y_cv, scProb, labels=c.classes_, eps=1e-15))
+        print("Log Loss: ", log_loss(y_cv, scProb))
+    
+ba = np.argmin(logerrorCV)
+c = LGBMClassifier(n_estimators=a[int(ba/4)], max_depth=md[int(ba%4)], random_state=42, n_jobs=-1)
+c.fit(x_train, y_train.values.ravel())
+sc = CalibratedClassifierCV(c, method="sigmoid")
+sc.fit(x_train, y_train.values.ravel())
+
+pred_y = sc.predict_proba(x_train)
+print('best alpha: ', a[int(ba/4)], "train log loss: ",
+      log_loss(y_train, pred_y, labels=c.classes_, eps=1e-15))
+pred_y = sc.predict_proba(x_cv)
+print("cv log loss: ",
+      log_loss(y_cv, pred_y, labels=c.classes_, eps=1e-15))
+pred_y = sc.predict_proba(X_test)
+print("test log loss: ",
+      log_loss(Y_test, pred_y, labels=c.classes_, eps=1e-15))
+
+c = LGBMClassifier(n_estimators=a[int(ba/4)], 
+                   max_depth=md[int(ba%4)],
+                   random_state=42,
+                   n_jobs=-1)
+c.fit(x_train, y_train.values.ravel())
+sc = CalibratedClassifierCV(c, method="sigmoid")
+sc.fit(x_train, y_train.values.ravel())
+
+res = sc.predict_proba(exam)
+outpA = []
+id = 200000
+
+for i in range(len(res)):
+    pred = res[i]
+    inA = [id, pred[0], pred[1], pred[2], pred[3],
+           pred[4], pred[5], pred[6], pred[7], pred[8]]
+    outpA.append(inA)
+    id += 1
+
+turner = pd.DataFrame(outpA, 
+                      columns=['id', 'Class_1', 'Class_2', 'Class_3',
+                               'Class_4', 'Class_5', 'Class_6', 'Class_7', 
+                               'Class_8', 'Class_9'])
+turner.to_csv(path_or_buf='submission2.csv', index = False)
 
 
-
-
-
-
-
-
-
-# RANDOM FORREST, initial accuracy: .35
-
-# rf = RandomForestClassifier(n_estimators=100)
-# rf.fit(X_train, Y_train)
-
-# y_pred = rf.predict(X_test)
-# print("Accuracy: ", metrics.accuracy_score(Y_test, y_pred))
-
-
-# DECISION TREE, initial accuracy: .23
-
-# from sklearn.tree import DecisionTreeClassifier
-# from sklearn.metrics import confusion_matrix
-
-# dtree_model = DecisionTreeClassifier().fit(X_train, Y_train)
-# dtree_predictions = dtree_model.predict(X_test)
-  
-# print("Accuracy: ", metrics.accuracy_score(Y_test, dtree_predictions))
-
-# SVM, initial accuracy: n/a
-  
-# training a linear SVM classifier
-# from sklearn.svm import SVC
-# svm_model_linear = SVC(kernel = 'linear', C = 1).fit(X_train, Y_train)
-# svm_predictions = svm_model_linear.predict(X_test)
-  
-# model accuracy for X_test  
-# accuracy = svm_model_linear.score(X_test, Y_test)
-# print("Accuracy: ", accuracy)
-
-#KNN, initial accuracy: .27
-
-# from sklearn.neighbors import KNeighborsClassifier
-# knn = KNeighborsClassifier(n_neighbors = 7).fit(X_train, Y_train)
-  
-# accuracy = knn.score(X_test, Y_test)
-# print(accuracy)
